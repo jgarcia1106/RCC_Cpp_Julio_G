b7062ebc6330390e42cd15e1b214074976b799dc
jgarcia1106 <catfish007@gmail.com>
2024-02-10 22:05:48 -0800

    completed checklist

--- Class/Assignment6_5_Grading_like_a_Scantron/nbproject/Package-Release.bash
+++ Class/Assignment6_5_Grading_like_a_Scantron/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment6_grading_like_a_scantron
-OUTPUT_BASENAME=assignment6_grading_like_a_scantron
-PACKAGE_TOP_DIR=assignment6gradinglikeascantron/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment6_5_grading_like_a_scantron
+OUTPUT_BASENAME=assignment6_5_grading_like_a_scantron
+PACKAGE_TOP_DIR=assignment65gradinglikeascantron/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/assignment6gradinglikeascantron/bin"
+makeDirectory "${NBTMPDIR}/assignment65gradinglikeascantron/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment6gradinglikeascantron.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment65gradinglikeascantron.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment6gradinglikeascantron.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment65gradinglikeascantron.tar *
 checkReturnCode
 
 # Cleanup
